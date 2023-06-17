d407d6660c1d11f28a388411d315a5f38025b0e9
Lokman-Hossain-Mujahid <rclash163@gmail.com>
2023-06-17 12:49:01 +0600

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>this is about AboutPage</h1>
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ src/app/partner/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Partner = () => {
+    return (
+        <div>
+            <h2>Partner With Us</h2>
+        </div>
+    );
+};
+
+export default Partner;
