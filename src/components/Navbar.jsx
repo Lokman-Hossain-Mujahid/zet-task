e72c78908ef04252e28a0080f80914ef50f85147
Lokman-Hossain-Mujahid <rclash163@gmail.com>
2023-06-20 03:17:22 +0600

    Active route added for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import React from 'react';
 import Logo from "../../../../images/Logo/zet_new_logo.7adcc993.svg"
 import Link from 'next/link';
+import NavLink from './NavLink';
 
 const navLinks = [
     {
@@ -26,13 +27,13 @@ const navLinks = [
 const Navbar = () => {
     return (
         <div className='bg-white py-2'>
-            <nav className='md:flex items-center md:justify-between w-[100vw] md:w-[90vw] mx-auto mt-4  '>
+            <nav className='md:flex items-center md:justify-between w-[100vw] md:w-[90vw] mx-auto md:mt-4  '>
                 <div className='flex justify-center'><Image className='h-14 w-24 ' src={Logo} alt='Logo' /></div>
                 <ul className='md:flex text-center items-center justify-center gap-5 text-lg text-gray-500'>
                     {
                         navLinks.map(({ path, title }) => (
                             <li key={path}>
-                                <Link href={path}>{title}</Link>
+                                <NavLink exact={path === "/"} activeClassName="text-blue-500" href={path}>{title}</NavLink>
                             </li>
                         ))
                     }
